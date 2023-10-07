27dd088fa52b602f2da3ad69b42bf3119e0dffd9
sajed013 <84460064+sajed013@users.noreply.github.com>
2023-10-07 12:11:02 +0530

    This is first commit

---
+++ DevOps/DevOpsSpringBootApp - Copy/src/main/java/com/cjc/hdfc/bank/DevOpsSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.hdfc.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world ");
+		SpringApplication.run(DevOpsSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ DevOps/DevOpsSpringBootApp - Copy/src/test/java/com/cjc/hdfc/bank/DevOpsSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.hdfc.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
